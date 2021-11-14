5a268e00db631abea0ecf72a68a32fa0eaf31dec
Gordon Brown <gbrown@spu.edu>
2021-11-14 14:53:59 -0700

    Solve day 4 part 2

--- 2020/day4/day4.lisp
+++ 2020/day4/day4.lisp
@@ -35,3 +35,37 @@
            (gethash '|pid| passport))
       t
       nil))
+
+(defun parse-int (str)
+  (if str
+      (parse-integer str :junk-allowed t)
+      nil))
+
+(defun validate-height (height)
+  (multiple-value-bind
+        (match groups) (ppcre:scan-to-strings "(\\d+)(in|cm)" height)
+    (when match
+      (let ((num (aref groups 0))
+            (unit (aref groups 1)))
+        (cond
+          ((equal "in" unit) (<= 59 (parse-int num) 76))
+          ((equal "cm" unit) (<= 150 (parse-int num) 193))
+          (t nil))))))
+
+(defun p2-passport-valid-p (passport)
+  (let ((byr (gethash '|byr| passport))
+        (iyr (gethash '|iyr| passport))
+        (eyr (gethash '|eyr| passport))
+        (hgt (gethash '|hgt| passport))
+        (hcl (gethash '|hcl| passport))
+        (ecl (gethash '|ecl| passport))
+        (pid (gethash '|pid| passport)))
+    (and
+     byr (<= 1920 (parse-int byr) 2002)
+     iyr (<= 2010 (parse-int iyr) 2020)
+     eyr (<= 2020 (parse-int eyr) 2030)
+     hgt (validate-height hgt)
+     hcl (ppcre:scan "^\#[0-9a-f]{6}$" hcl)
+     ecl (ppcre:scan "amb|blu|brn|gry|grn|hzl|oth" ecl)
+     pid (ppcre:scan "^[0-9]{9}$" pid)
+     t)))
--- 2020/day4/day4tests.lisp
+++ 2020/day4/day4tests.lisp
@@ -7,7 +7,21 @@
           :count (passport-valid-p passport))))
 
 (checkl:check (:name :part-one)
-  (let* ((sample-input (file-string #p"./day4input.lisp"))
-         (passports (parse-passports sample-input)))
+  (let* ((input (file-string #p"./day4input.lisp"))
+         (passports (parse-passports input)))
     (loop :for passport :in passports
           :count (passport-valid-p passport))))
+
+(checkl:check (:name :part-two-valid-samples)
+  (mapcar #'p2-passport-valid-p
+          (parse-passports (file-string #p"./day4-p2-sample.txt"))))
+
+(checkl:check (:name :part-two-invalid-samples)
+  (mapcar #'p2-passport-valid-p
+          (parse-passports (file-string #p"./day4-p2-invalid-sample.txt"))))
+
+(checkl:check (:name :part-two)
+  (let* ((input (file-string #p"./day4input.lisp"))
+         (passports (parse-passports input)))
+    (loop :for passport :in passports
+          :count (p2-passport-valid-p passport))))
