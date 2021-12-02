6164a9a3211b1d94cc8bee1c4ede59196b338eec
Gordon Brown <gbrown@spu.edu>
2021-12-02 15:36:23 -0700

    Solve day 1 part 2

--- 2021/day1/day1.lisp
+++ 2021/day1/day1.lisp
@@ -4,8 +4,8 @@
 
 
 (defun read-int-list (in)
-  (do ((input-numbers '() (push (read in nil 'done) input-numbers)))
-      ((eq 'done (first input-numbers)) (rest input-numbers))))
+  (nreverse (do ((input-numbers '() (push (read in nil 'done) input-numbers)))
+                ((eq 'done (first input-numbers)) (rest input-numbers)))))
 
 (defun count-depth-increases (depths)
   (loop :for remaining :on depths
@@ -13,10 +13,23 @@
                      (second (second remaining)))
                  (if (null second)
                      nil
-                     (> first second)))))
+                     (< first second)))))
+
+(defun count-depth-increases* (depths)
+  (let* ((doubles (mapcar #'list depths (nthcdr 1 depths)))
+         (increases (mapcar (lambda (x) (< (first x) (second x))) doubles)))
+    (count-if #'identity increases)))
+
+(defun sum-triples (depths)
+  (let* ((triples (mapcar #'list depths (nthcdr 1 depths) (nthcdr 2 depths)))
+         (sums (mapcar (lambda (triple) (apply #'+ triple)) triples)))
+    sums))
 
 (with-open-file (in #p"./day1sample.txt")
-  (count-depth-increases (read-int-list in)))
+  (count-depth-increases* (read-int-list in)))
 
 (with-open-file (in #p"./day1input.txt")
-  (count-depth-increases (read-int-list in)))
+  (count-depth-increases* (read-int-list in)))
+
+(with-open-file (in #p"./day1input.txt")
+  (count-depth-increases (sum-triples (read-int-list in))))
