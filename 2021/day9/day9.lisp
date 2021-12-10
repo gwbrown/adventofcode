bc347fe9881f7fca345b93bae394bd85d5c15646
Gordon Brown <gbrown@spu.edu>
2021-12-10 11:32:27 -0700

    Solve day 9 part 2

--- 2021/day9/day9.lisp
+++ 2021/day9/day9.lisp
@@ -60,3 +60,63 @@
       ;; (break)
       sum)))
 
+(defun bounded-aref (map row col)
+  "Similar to `aref', but if the row or column would be out of bounds,
+returns 9 (the max map height) instead."
+  (let ((max-row (1- (array-dimension map 0)))
+        (max-col (1- (array-dimension map 1))))
+    (if (or (not (<= 0 row max-row))
+            (not (<= 0 col max-col)))
+        9 ;; Hardcoded for this problem 
+        (aref map row col))))
+
+(defun bounded-aref* (map point)
+  (destructuring-bind (row . col) point
+    (bounded-aref map row col)))
+
+(defun step-basin (map origin)
+  "Returns a list of the points adjacent to `origin' that are part
+of the same basin, not including `origin'."
+  (destructuring-bind (row . col) origin
+    (let ((up (cons (1- row) col))
+          (down (cons (1+ row) col))
+          (left (cons row (1- col)))
+          (right (cons row (1+ col)))
+          (basin-points '()))
+      (flet ((push-if-in-basin (point)
+               (when (> 9 (bounded-aref* map point))
+                 (push point basin-points))))
+        (push-if-in-basin up)
+        (push-if-in-basin down)
+        (push-if-in-basin left)
+        (push-if-in-basin right)
+        basin-points))))
+
+(defun explore-basin (map origin)
+  (flet ((step-this-basin (pt) (step-basin map pt)))
+    (do* ((known-basin '() (nunion known-basin frontier :test #'equal))
+          (frontier (list origin) next-frontier)
+          (next-frontier (step-this-basin origin)
+                         (delete-if (lambda (pt)
+                                      (or (member pt known-basin :test #'equal)
+                                          (member pt next-frontier :test #'equal)))
+                                    (delete-duplicates
+                                     (mapcan #'step-this-basin frontier)
+                                     :test #'equal))))
+         ((null next-frontier) (nunion known-basin frontier :test #'equal)))))
+
+
+(defun solve-part-two ()
+  (with-open-file (in #p"./input.txt")
+    (let* ((lines (read-lines in))
+           (map (parse-map lines))
+           (lowest-points (find-lowest-points map))
+           (basins (mapcar (lambda (origin)
+                             (explore-basin map origin))
+                           lowest-points))
+           (basins-by-size (sort (copy-tree basins)
+                                 (lambda (a b) (> (length a) (length b))))))
+      (break)
+      (* (length (first basins-by-size))
+         (length (third basins-by-size))
+         (length (second basins-by-size))))))
